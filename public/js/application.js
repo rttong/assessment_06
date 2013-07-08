e60d19306da10073ae1b6e68bdfc2420a7230f32
rttong <eufonik@hotmail.com>
2013-07-07 17:35:23 -0700

    Update application.js

--- public/js/application.js
+++ public/js/application.js
@@ -2,7 +2,8 @@ $(document).ready(function(){
 	$('#get_color').on('click', function() {
 		$.ajax({
 			type: 'POST',
-			url: '/color'
+			url: '/color',
+			dataType: 'json'
 		}).done(function(response){
 			console.log(response.cell);
 			$('#color_me').children('li').eq(response.cell).css('background-color', response.color);
